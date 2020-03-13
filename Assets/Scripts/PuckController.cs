761b3261ec3288143f90872c782652a2c2bac77f
garlicsuter <markasuter@gmail.com>
2020-03-13 09:37:03 -0400

    pucks move

--- Assets/Scripts/PuckController.cs
+++ Assets/Scripts/PuckController.cs
@@ -4,45 +4,44 @@ using UnityEngine;
 
 public class PuckController : MonoBehaviour
 {
-    public float xRange;
-    public float yRange;
-    //public int[] points = { 7, 5, 1, 0 };
-    public int[] direction = {0,1,2,3};
+    public int[] direction = { 0, 1, 2, 3 };
     public int moveDirection;
     public float puckSpeed = 5.0f;
- 
 
     // Start is called before the first frame update
     void Start()
     {
-        
         moveDirection = direction[Random.Range(0,4)];
-        Debug.Log("moveDirection: " + moveDirection);
+        Debug.Log("moveDirection = " + moveDirection);
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        if (moveDirection == 0)
+        if(moveDirection == 0)
         {
             transform.Translate(transform.up * puckSpeed * Time.deltaTime);
         }
+
         if (moveDirection == 1)
         {
             transform.Translate(transform.right * puckSpeed * Time.deltaTime);
         }
+
         if (moveDirection == 2)
         {
             transform.Translate(transform.up * -puckSpeed * Time.deltaTime);
         }
+
         if (moveDirection == 3)
         {
             transform.Translate(transform.right * -puckSpeed * Time.deltaTime);
         }
-
-
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("WALLLLLLLLLLLLLLLLLLLL");
+        puckSpeed = -puckSpeed;
+    }
 }
