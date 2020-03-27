29926b388b5c26fb2b57cf983220ec7abfd4be3d
garlicsuter <markasuter@gmail.com>
2020-03-27 12:51:57 -0400

    score counter

--- Assets/Scripts/PlayerController02.cs
+++ Assets/Scripts/PlayerController02.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController02 : MonoBehaviour
 {
     public float speed;
@@ -9,12 +10,16 @@ public class PlayerController02 : MonoBehaviour
     public float yRange;
     public GameObject Puck;
     public GameObject Blocky;
+    //public int Score;
+    public GameObject scoreText;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
-
+        //Score = 0;
 
     }
     void LateUpdate()
@@ -45,8 +50,7 @@ public class PlayerController02 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Instantiate(Puck,new Vector2(Random.Range(-xRange,xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
-
+        
         //count how many enemies there are in the scene
         GameObject[] puckArray;
         puckArray = GameObject.FindGameObjectsWithTag("Puck");
@@ -65,4 +69,18 @@ public class PlayerController02 : MonoBehaviour
         transform.Translate(movement * speed * Time.deltaTime);
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Blocky")){
+            Debug.Log("You hit a blocky!!");
+            Destroy(other.gameObject);
+            Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+            //Score += 5;
+            //Debug.Log("Your Score: " + Score);
+
+            scoreText.GetComponent<ScoreKeeper>().UpdateScore();
+        }
+
+        
+    }
 }
