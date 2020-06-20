5d68a60e9096f8930b91ec9e9d6ec2927e3b2f1d
garlicsuter <markasuter@gmail.com>
2020-06-20 10:08:17 -0400

    Room Switching Directional Spawn Issues
    Room Switching

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+ 
+    void Start()
+    {
+        StartCoroutine(ABC());
+    }
+
+    IEnumerator ABC()
+    {
+
+        //returning 0 will make it wait 1 frame
+        yield return 0;
+
+        bool playerExists = (GameObject.Find("player") != null);
+        if (playerExists == false)
+        {
+            Instantiate(player, new Vector2(-4, -4), Quaternion.identity);
+        }
+        
+
+
+    }
+
+}
